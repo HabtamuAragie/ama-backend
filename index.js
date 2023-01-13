88d89818d0d611bc406a3d54fdded2ccd8964905
HabtamuAragie <habtamuaragie700@gmail.com>
2023-01-13 11:31:50 -0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const functions = require("firebase-functions");
+// const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(
@@ -7,7 +7,7 @@ const stripe = require("stripe")(
 // - App config
 const app = express();
 // - Middlewares
-app.use(cors({ origin: true }));
+app.use(cors());
 app.use(express.json());
 app.get("/", (request, response) => response.status(200).send("hello world"));
 
